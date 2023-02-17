a4eef842b2e7b12b87ddae65b5d291c4c23c6969
skogarr <apkarp@gmail.com>
2023-02-17 19:53:28 +0100

    Fixed a typo in the creation script

---
+++ db/insert_script.sql
@@ -0,0 +1,24 @@
+start transaction;
+insert into cities(name) values
+('Auroraville');
+insert into streets(name, city_id) values
+('Maplewood Drive',1),
+('Juniper Way',1),
+('Chestnut Ave',1),
+('Poplar Crescent',1),
+('Elmwood Lane',1),
+('Laurel Street',1),
+('Magnolia Court',1),
+('Spruce Lane',1),
+('Pinehurst Place',1),
+('Cherry Blossom Road',1),
+('Birchwood Street',1),
+('Hawthorn Court',1),
+('Oakwood Boulevard',1),
+('Acacia Drive',1),
+('Willowdale Road',1),
+('Dogwood Avenue',1),
+('Sycamore Circle',1),
+('Aspen View Lane',1),
+('Redwood Terrace',1),
+('Cedar Ridge Lane',1);
