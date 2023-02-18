11f69314211eb5ad49e9a19eac8e08eabf8b5a9e
Paw3lUlita <pawel.ulita33@gmail.com>
2023-02-18 17:38:00 +0100

    refactoring + dependencies

--- Laba-Navigation-Project/src/main/java/org/laba/dao/ITransitPointDAO.java
+++ Laba-Navigation-Project/src/main/java/org/laba/dao/ITransitPointDAO.java
@@ -2,5 +2,8 @@ package org.laba.dao;
 
 import org.laba.model.TransitPoint;
 
+import java.util.List;
+
 public interface ITransitPointDAO extends IBaseDAO<TransitPoint>{
+    List<TransitPoint> getAllTransitPoints();
 }
