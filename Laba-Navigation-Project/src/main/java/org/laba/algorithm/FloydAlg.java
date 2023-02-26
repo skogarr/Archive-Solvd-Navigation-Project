06872f7299e4980d0d37459aec5c7521536bc9e6
kacper.zukowski <kacper.zukowski@sensilabs.pl>
2023-02-26 14:36:38 +0100

    added cli interface

--- Laba-Navigation-Project/src/main/java/org/laba/algorithm/FloydAlg.java
+++ Laba-Navigation-Project/src/main/java/org/laba/algorithm/FloydAlg.java
@@ -46,62 +46,9 @@ public class FloydAlg {
     this.allTransitPoints = allTransitPoints;
   }
 
-  private void compute() {
-    this.distance = new double[graph.length][graph.length];
-    this.next = new double[graph.length][graph.length];
-    init(graph);
-    floyd(graph.length);
-  }
-
   public void setGraph(double[][] graph) {
     this.graph = graph;
     compute();
-  }
-
-  private void init(double[][] graph) {
-    int V = graph.length;
-    for (int i = 0; i < V; i++) {
-      for (int j = 0; j < V; j++) {
-        distance[i][j] = graph[i][j];
-        if (graph[i][j] == X) {
-          next[i][j] = -1;
-        } else {
-          next[i][j] = j;
-        }
-      }
-    }
-  }
-
-  private ArrayList<Integer> constructPath(int from, int to) {
-    if (this.graph == null) {
-      throw new NullPointerException("Variable \"graph\" wasn't set");
-    }
-    if (next[from][to] == -1) {
-      return null;
-    }
-    ArrayList<Integer> path = new ArrayList<>();
-    path.add(from);
-    while (from != to) {
-      from = (int) next[from][to];
-      path.add(from);
-    }
-    return path;
-  }
-
-  private void floyd(int V) {
-    for (int k = 0; k < V; k++) {
-      for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-          if (distance[i][k] == X || distance[k][j] == X) {
-            continue;
-          }
-          if (distance[i][j] > distance[i][k] + distance[k][j]) {
-            distance[i][j] = distance[i][k] + distance[k][j];
-            next[i][j] = next[i][k];
-          }
-        }
-      }
-    }
   }
 
   public ArrayList<Integer> getPath(int fromLoc, int toLoc) {
@@ -150,7 +97,6 @@ public class FloydAlg {
           continue;
         }
         double distance = getDistance(i, j);
-
         double busWeightab = getBusWeight(i, j);
         if (busWeightab == -1d || busWeightab == 0.0) {
           busWeightab = Double.MAX_VALUE;
@@ -163,7 +109,6 @@ public class FloydAlg {
         if (metroWeightab == -1d || metroWeightab == 0.0) {
           metroWeightab = Double.MAX_VALUE;
         }
-
         double busWeightba = getBusWeight(j, i);
         if (busWeightba == -1d || busWeightba == 0.0) {
           busWeightba = Double.MAX_VALUE;
@@ -176,23 +121,19 @@ public class FloydAlg {
         if (metroWeightba == -1d || metroWeightba == 0.0) {
           metroWeightba = Double.MAX_VALUE;
         }
-
         if (Math.min(Math.min(busWeightab, tramWeightab), metroWeightab) == Double.MAX_VALUE) {
           graph[i][j] = Integer.MAX_VALUE;
           continue;
         }
-
         if (Math.min(Math.min(busWeightba, tramWeightba), metroWeightba) == Double.MAX_VALUE) {
           graph[j][i] = Integer.MAX_VALUE;
           continue;
         }
-
         double valueab = Math.min(busWeightab * distance,
             Math.min(tramWeightab * distance, metroWeightab * distance));
 
         double valueba = Math.min(busWeightba * distance,
             Math.min(tramWeightba * distance, metroWeightba * distance));
-
         graph[i][j] = valueab;
         graph[j][i] = valueba;
       }
@@ -229,7 +170,103 @@ public class FloydAlg {
       }
       sum += value * distance;
     }
-    return Double.parseDouble(String.format(Locale.US,"%.2f", sum));
+    return Double.parseDouble(String.format(Locale.US, "%.2f", sum));
+  }
+
+
+  public void prettyPrintPublic(ArrayList<Integer> path) {
+    for (int i = 0; i < path.size() - 1; i++) {
+      Integer transitPointA = path.get(i);
+      Integer transitPointB = path.get(i + 1);
+      double busWeight = getBusWeight(transitPointA, transitPointB);
+      if (busWeight == 0.0d) {
+        busWeight = Double.MAX_VALUE;
+      }
+      double tramWeight = getTramWeight(transitPointA, transitPointB);
+      if (tramWeight == 0.0d) {
+        tramWeight = Double.MAX_VALUE;
+      }
+      double metroWeight = getMetroWeight(transitPointA, transitPointB);
+      if (metroWeight == 0.0d) {
+        metroWeight = Double.MAX_VALUE;
+      }
+      if ((busWeight < tramWeight) && (busWeight < metroWeight)) {
+        List<BusStop> allByTransitPointAId = busStopService.getAllByTransitPointId(
+            Long.valueOf(transitPointA));
+        List<BusStop> allByTransitPointBId = busStopService.getAllByTransitPointId(
+            Long.valueOf(transitPointB));
+        long busRoute = allByTransitPointAId.stream()
+            .map(x -> x.getBusRoute().getId())
+            .filter(allByTransitPointBId.stream().map(x -> x.getBusRoute().getId())
+                .collect(Collectors.toSet())::contains)
+            .findFirst()
+            .orElse(-1L);
+        String stopNo1 = allByTransitPointAId.stream()
+            .filter(x -> x.getBusRoute().getId() == busRoute)
+            .map(BusStop::getStopNo)
+            .findFirst()
+            .orElse("UNKNOWN");
+        String stopNo2 = allByTransitPointBId.stream()
+            .filter(x -> x.getBusRoute().getId() == busRoute)
+            .map(BusStop::getStopNo)
+            .findFirst()
+            .orElse("UNKNOWN");
+        System.out.printf("Go from transit point %s, bus stop number %s, bus route %s,"
+                + "to transit point %s, bus stop number %s\n",
+            transitPointA, stopNo1, busRoute, transitPointB, stopNo2);
+      } else if ((tramWeight < busWeight) && (tramWeight < metroWeight)) {
+        List<TramStop> allByTransitPointAId = tramStopService.getAllByTransitPointId(
+            Long.valueOf(transitPointA));
+        List<TramStop> allByTransitPointBId = tramStopService.getAllByTransitPointId(
+            Long.valueOf(transitPointB));
+        long tramRoute = allByTransitPointAId.stream()
+            .map(x -> x.getTramRoute().getId())
+            .filter(allByTransitPointBId.stream().map(x -> x.getTramRoute().getId())
+                .collect(Collectors.toSet())::contains)
+            .findFirst()
+            .orElse(-1L);
+        int stopNo1 = allByTransitPointAId.stream()
+            .filter(x -> x.getTramRoute().getId() == tramRoute)
+            .map(TramStop::getStopNo)
+            .findFirst()
+            .orElse(-1);
+        int stopNo2 = allByTransitPointBId.stream()
+            .filter(x -> x.getTramRoute().getId() == tramRoute)
+            .map(TramStop::getStopNo)
+            .findFirst()
+            .orElse(-1);
+        System.out.printf("Go from transit point %s, tram stop number %s, tram route %s,"
+                + "to transit point %s, tram stop number %s\n",
+            transitPointA, stopNo1, tramRoute, transitPointB, stopNo2);
+      } else if ((metroWeight < busWeight) && (metroWeight < tramWeight)) {
+        List<MetroStop> allByTransitPointAId = metroStopService.getAllByTransitPointId(
+            Long.valueOf(transitPointA));
+        List<MetroStop> allByTransitPointBId = metroStopService.getAllByTransitPointId(
+            Long.valueOf(transitPointB));
+        long metroRoute = allByTransitPointAId.stream()
+            .map(x -> x.getMetroRoute().getId())
+            .filter(allByTransitPointBId.stream().map(x -> x.getMetroRoute().getId())
+                .collect(Collectors.toSet())::contains)
+            .findFirst()
+            .orElse(-1L);
+        int stopNo1 = allByTransitPointAId.stream()
+            .filter(x -> x.getMetroRoute().getId() == metroRoute)
+            .map(MetroStop::getStopNo)
+            .findFirst()
+            .orElse(-1);
+        int stopNo2 = allByTransitPointBId.stream()
+            .filter(x -> x.getMetroRoute().getId() == metroRoute)
+            .map(MetroStop::getStopNo)
+            .findFirst()
+            .orElse(-1);
+        System.out.printf("Go from transit point %s, metro stop number %s, metro route %s,"
+                + "to transit point %s, metro stop number %s\n",
+            transitPointA, stopNo1, metroRoute, transitPointB, stopNo2);
+      } else {
+        System.out.print("nothing -> ");
+      }
+    }
+    System.out.println("You have arrived at destination");
   }
 
   private double getDistance(int i, int j) {
@@ -290,115 +327,56 @@ public class FloydAlg {
     return weight;
   }
 
-  public void prettyPrintPublic(ArrayList<Integer> path) {
-    for (int i = 0; i < path.size() - 1; i++) {
-      Integer transitPointA = path.get(i);
-      Integer transitPointB = path.get(i + 1);
-
-      double busWeight = getBusWeight(transitPointA, transitPointB);
-      if (busWeight == 0.0d) {
-        busWeight = Double.MAX_VALUE;
+  private void init(double[][] graph) {
+    int V = graph.length;
+    for (int i = 0; i < V; i++) {
+      for (int j = 0; j < V; j++) {
+        distance[i][j] = graph[i][j];
+        if (graph[i][j] == X) {
+          next[i][j] = -1;
+        } else {
+          next[i][j] = j;
+        }
       }
-      double tramWeight = getTramWeight(transitPointA, transitPointB);
-      if (tramWeight == 0.0d) {
-        tramWeight = Double.MAX_VALUE;
-      }
-      double metroWeight = getMetroWeight(transitPointA, transitPointB);
-      if (metroWeight == 0.0d) {
-        metroWeight = Double.MAX_VALUE;
-      }
-
-      if ((busWeight < tramWeight) && (busWeight < metroWeight)) {
-        List<BusStop> allByTransitPointAId = busStopService.getAllByTransitPointId(
-            Long.valueOf(transitPointA));
-
-        List<BusStop> allByTransitPointBId = busStopService.getAllByTransitPointId(
-            Long.valueOf(transitPointB));
-
-        long busRoute = allByTransitPointAId.stream()
-            .map(x -> x.getBusRoute().getId())
-            .filter(allByTransitPointBId.stream().map(x -> x.getBusRoute().getId())
-                .collect(Collectors.toSet())::contains)
-            .findFirst()
-            .orElse(-1L);
-
-        String stopNo1 = allByTransitPointAId.stream()
-            .filter(x -> x.getBusRoute().getId() == busRoute)
-            .map(BusStop::getStopNo)
-            .findFirst()
-            .orElse("UNKNOWN");
-        String stopNo2 = allByTransitPointBId.stream()
-            .filter(x -> x.getBusRoute().getId() == busRoute)
-            .map(BusStop::getStopNo)
-            .findFirst()
-            .orElse("UNKNOWN");
-
-        System.out.printf("Go from transit point %s, bus stop number %s, bus route %s,"
-                + "to transit point %s, bus stop number %s\n",
-            transitPointA, stopNo1, busRoute, transitPointB, stopNo2);
-      } else if ((tramWeight < busWeight) && (tramWeight < metroWeight)) {
-
-        List<TramStop> allByTransitPointAId = tramStopService.getAllByTransitPointId(
-            Long.valueOf(transitPointA));
-        List<TramStop> allByTransitPointBId = tramStopService.getAllByTransitPointId(
-            Long.valueOf(transitPointB));
-
-        long tramRoute = allByTransitPointAId.stream()
-            .map(x -> x.getTramRoute().getId())
-            .filter(allByTransitPointBId.stream().map(x -> x.getTramRoute().getId())
-                .collect(Collectors.toSet())::contains)
-            .findFirst()
-            .orElse(-1L);
-
-        int stopNo1 = allByTransitPointAId.stream()
-            .filter(x -> x.getTramRoute().getId() == tramRoute)
-            .map(TramStop::getStopNo)
-            .findFirst()
-            .orElse(-1);
-        int stopNo2 = allByTransitPointBId.stream()
-            .filter(x -> x.getTramRoute().getId() == tramRoute)
-            .map(TramStop::getStopNo)
-            .findFirst()
-            .orElse(-1);
-
-        System.out.printf("Go from transit point %s, tram stop number %s, tram route %s,"
-                + "to transit point %s, tram stop number %s\n",
-            transitPointA, stopNo1, tramRoute, transitPointB, stopNo2);
-      } else if ((metroWeight < busWeight) && (metroWeight < tramWeight)) {
-
-        List<MetroStop> allByTransitPointAId = metroStopService.getAllByTransitPointId(
-            Long.valueOf(transitPointA));
-        List<MetroStop> allByTransitPointBId = metroStopService.getAllByTransitPointId(
-            Long.valueOf(transitPointB));
-
-        long metroRoute = allByTransitPointAId.stream()
-            .map(x -> x.getMetroRoute().getId())
-            .filter(allByTransitPointBId.stream().map(x -> x.getMetroRoute().getId())
-                .collect(Collectors.toSet())::contains)
-            .findFirst()
-            .orElse(-1L);
-
-        int stopNo1 = allByTransitPointAId.stream()
-            .filter(x -> x.getMetroRoute().getId() == metroRoute)
-            .map(MetroStop::getStopNo)
-            .findFirst()
-            .orElse(-1);
-        int stopNo2 = allByTransitPointBId.stream()
-            .filter(x -> x.getMetroRoute().getId() == metroRoute)
-            .map(MetroStop::getStopNo)
-            .findFirst()
-            .orElse(-1);
-
-        System.out.printf("Go from transit point %s, metro stop number %s, metro route %s,"
-                + "to transit point %s, metro stop number %s\n",
-            transitPointA, stopNo1, metroRoute, transitPointB, stopNo2);
-      } else {
-        System.out.print("nothing -> ");
-      }
-
-
     }
-    System.out.println("You have arrived at destination");
   }
 
+  private ArrayList<Integer> constructPath(int from, int to) {
+    if (this.graph == null) {
+      throw new NullPointerException("Variable \"graph\" wasn't set");
+    }
+    if (next[from][to] == -1) {
+      return null;
+    }
+    ArrayList<Integer> path = new ArrayList<>();
+    path.add(from);
+    while (from != to) {
+      from = (int) next[from][to];
+      path.add(from);
+    }
+    return path;
+  }
+
+  private void floyd(int V) {
+    for (int k = 0; k < V; k++) {
+      for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+          if (distance[i][k] == X || distance[k][j] == X) {
+            continue;
+          }
+          if (distance[i][j] > distance[i][k] + distance[k][j]) {
+            distance[i][j] = distance[i][k] + distance[k][j];
+            next[i][j] = next[i][k];
+          }
+        }
+      }
+    }
+  }
+
+  private void compute() {
+    this.distance = new double[graph.length][graph.length];
+    this.next = new double[graph.length][graph.length];
+    init(graph);
+    floyd(graph.length);
+  }
 }
--- Laba-Navigation-Project/src/main/java/org/laba/model/TripType.java
+++ Laba-Navigation-Project/src/main/java/org/laba/model/TripType.java
@@ -1,5 +1,8 @@
 package org.laba.model;
 
+import lombok.ToString;
+
+@ToString
 public enum TripType {
   PRIVATE, PUBLIC
 }
